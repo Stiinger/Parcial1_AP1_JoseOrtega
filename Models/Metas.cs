c38a78ea7a18bbdcbbd60a3ccf74aceb8f4abef0
SirLevid <jassontejada@gmail.com>
2024-02-12 18:40:35 -0400

    Agregando modelo Metas

---
+++ Models/Metas.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_AP1_JoseOrtega.Models;
+
+public class Metas
+{
+    [Key]
+    public int MetaId { get; set; }
+    [Required(ErrorMessage = "Es obligatorio introducir la descripción.")]
+    public string Descripcion { get; set; } = string.Empty;
+    public DateTime Fecha { get; set; }
+    [Range(1, 1000000000, ErrorMessage = "El monto debe ser mayor a 0.")]
+    public decimal Monto { get; set; }
+}
